5a46bfa44df4795bdeb18af65245a41b0437b124
tcstl95 <tomcarlson171@gmail.com>
2022-09-28 23:28:24 -0500

    Commit to push for BCS

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var capitalLetters = ["T", "D"];
+var lowerLetters = ["g", "x"];
+var numbers = ["2","6"];
+var specialCharacters = ["!", "%"];
+var answers = ["Tg2!","Tx2!", "Tx2%","Tg6!","Tg2%","Tg6%","Dg2!","Dg2%","Dx2!","Dx6!","Dx2%","Dx6%",];
+
 
 // Write password to the #password input
-function generatePassword(){
-   userChoice = window.prompt("Choose length of password. Between 8 - 128 characters.");
-   userChoice = window.prompt("Now choose type. uppercase, lowercase, numeric, special characters");
-}
-
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+ userChoice = window.prompt("Choose length of password. Must be between 8 and 128 characters");
+ userChoice = window.prompt("Choose capital letter: T,D");
+ userChoice = window.prompt("Choose lowercase letter: g, x" );
+ userChoice = window.prompt("Choose a number: 2,6");
+ userChoice = window.prompt("Choose a special character: !, %");
 
-  passwordText.value = password;
-
+ 
 }
+
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword,);
+generateBtn.addEventListener("click", writePassword);
