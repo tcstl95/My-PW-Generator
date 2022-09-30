79f588fd23729233f00801c2a89264ca52ad1ef1
tcstl95 <tomcarlson171@gmail.com>
2022-09-29 22:25:16 -0500

    Commit for bcs

--- assets/script.js
+++ assets/script.js
@@ -4,21 +4,29 @@ var capitalLetters = ["T", "D"];
 var lowerLetters = ["g", "x"];
 var numbers = ["2","6"];
 var specialCharacters = ["!", "%"];
-var answers = ["Tg2!","Tx2!", "Tx2%","Tg6!","Tg2%","Tg6%","Dg2!","Dg2%","Dx2!","Dx6!","Dx2%","Dx6%",];
+var password = ["Tg1!", ""]
+
 
 
 // Write password to the #password input
 function writePassword() {
  userChoice = window.prompt("Choose length of password. Must be between 8 and 128 characters");
- userChoice = window.prompt("Choose capital letter: T,D");
- userChoice = window.prompt("Choose lowercase letter: g, x" );
- userChoice = window.prompt("Choose a number: 2,6");
- userChoice = window.prompt("Choose a special character: !, %");
+ userChoice = window.prompt("Do you want Capital letters?");
+ userChoice = window.prompt("Do you want lowercase letters?" );
+ userChoice = window.prompt("Do you want a number?");
+ userChoice = window.prompt("Do you want a special character?");
 
- 
+
+var index = Math.floor(Math.random()* answers.length);
+computerChoice = answers[index];
+
+window.alert("Here is your password " + computerChoice); 
+
+
 }
 
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
