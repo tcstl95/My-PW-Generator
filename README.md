d5410a03cb47095f3ec5986e7176e14a02f8d964
tcstl95 <tomcarlson171@gmail.com>
2022-10-02 21:30:16 -0500

    Commit with final code for submission

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# My-PW-Generator-
+# My-PW-Generator
+
+# Description
+This my submission for a password generator.
+
+# Usage
+
+Anyone can use this generator in order to create a password that is strong and complex. 
+This will help with security and making sure no one can hack a users account.
+
+
+
+# Screenshot
+<img width="858" alt="PW Generator" src="https://user-images.githubusercontent.com/107820740/193490934-0daf5699-15db-4d02-a3e6-534c4ef7afd7.png">
--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var capitalLetters = ["A","B","C", "D","E","F","G","H","I","J","K","L","M","N","O","P"];
-var lowerLetters = ["a","b","c", "d","e","f","g","h","i","j","k","l","m","n","o","p"];
+var capitalLetters = ["A","B","C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q", "R","S","T", "U","V", "W","X","Y","Z"];
+var lowerLetters = ["a","b","c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharacters = ["!", "%","*","$","&"];
+var pickChoices;
+var passwordArray =[];
 
 
 
 
 // Write password to the #password input
 function writePassword() {
-
+    
 
 var password = generatePassword();
 var passwordText = document.querySelector("#password");
@@ -22,31 +24,42 @@ var passwordText = document.querySelector("#password");
 }
 
 function generatePassword(){
-    empArray=[];
-    result = prompt("How many characters? Between 8 and 128");
+   var empArray=[];
+    var result = parseInt(prompt("How many characters? Between 8 and 128"));
+    console.log (result);
+    console.log (typeof(result));
     if(isNaN(result) || result < 8 || result > 128){
         alert("Must have 8 to 128 characters!");
-        return false
+        return;
     }
     if (confirm("Do you want numbers? ")){
         empArray= empArray.concat(numbers);
+        console.log (empArray);
     }
     if(confirm("Do you want lowercase letters?")){
         empArray= empArray.concat(lowerLetters);
+        console.log (empArray);
     }
     if (confirm("Do you want uppercase letters?")){
         empArray= empArray.concat(capitalLetters);
+        console.log (empArray);
     }
     if (confirm("Do you want special characters?")){
         empArray= empArray.concat(specialCharacters);
+        console.log (empArray);
+    }
+    
+        for (var i=0; i <result; i++){
+            var pickChoices =empArray[Math.floor(Math.random()* empArray.length)];
+            passwordArray.push(pickChoices);
+        }
+        return passwordArray.join("")
+    
+
+   
     
     }
-    console.log(empArray);
-    return true
-    }
-
-
-
+ 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
